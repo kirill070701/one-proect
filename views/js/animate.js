3ec63b90b5b0dc1ffa3728936e581302f7bc535d
”Kirill <kirill07.07.01@mail.ru>
2020-12-29 20:34:32 +0300

    плавное появления слоя

---
+++ views/js/animate.js
@@ -0,0 +1,8 @@
+$(function () {
+    var element = $("#hid"), display;
+    $(window).scroll(function () {
+      display = $(this).scrollTop() >= 1350;
+      display != element.css('opacity') && element.stop().animate({ 'opacity': display }, 400);
+    });
+
+  });
