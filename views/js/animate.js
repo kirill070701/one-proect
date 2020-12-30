b3b4d6106df7ecdcb4a86ff401a437a9d4afe892
”Kirill <kirill07.07.01@mail.ru>
2020-12-30 21:10:07 +0300

    обработаны и обрезаны картинки на фоне

--- views/js/animate.js
+++ views/js/animate.js
@@ -1,8 +1,10 @@
 $(function () {
     var element = $("#hid"), display;
     $(window).scroll(function () {
-      display = $(this).scrollTop() >= 1350;
-      display != element.css('opacity') && element.stop().animate({ 'opacity': display }, 350);
+      display = $(this).scrollTop() >= 600;
+      display != element.css('opacity') && element.stop().animate({  'opacity': 0.5  }, 350) ;
     });
 
   });
+
+
