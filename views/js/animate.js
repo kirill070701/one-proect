b99d3284596db93d03452e2ac65f4429fccce86d
”Kirill <kirill07.07.01@mail.ru>
2020-12-30 09:56:52 +0300

    доработать тень на мобильной версии

--- views/js/animate.js
+++ views/js/animate.js
@@ -2,7 +2,7 @@ $(function () {
     var element = $("#hid"), display;
     $(window).scroll(function () {
       display = $(this).scrollTop() >= 1350;
-      display != element.css('opacity') && element.stop().animate({ 'opacity': display }, 400);
+      display != element.css('opacity') && element.stop().animate({ 'opacity': display }, 350);
     });
 
   });
