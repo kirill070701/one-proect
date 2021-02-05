64cc68fff2ff502b83eca8db1f1988b9ba1796ca
”Kirill <kirill07.07.01@mail.ru>
2021-02-05 23:16:49 +0300

    Сделать функцию ассинхронной

--- views/js/socket.js
+++ views/js/socket.js
@@ -1,13 +1,40 @@
 const express = require('express')
 const app = express()
-const http = require('http').Server(app);
-const io = require('socket.io')(http);
-module.exports = function(io){ 
-    console.log('podcklycheno')
-    io.on('connection', (socket) => {
-    
-        console.log("socket ok")
-        socket.emit('one', x);
-    
-    });
+const layouts = require("../../routes/gaming-industry-layout")
+
+module.exports = {
+        start: async function(io){ 
+            for (; ; ) {
+                setTimeout(() => {
+                    console.log('podcklycheno')
+                    io.on('connection', (socket) => {
+                
+                        const url = 'mongodb+srv://Admin:159963357741@cluster0.mjotf.mongodb.net/statie'
+                        const MongoClient = require('mongodb').MongoClient
+                    
+                        MongoClient.connect(url, (err, db)=>{
+                            if(err) throw err;
+                            var dbo = db.db("newdb")
+                    
+                            dbo.collection("students").find({}).sort({_id:-1}).limit(3).toArray((err, results)=>{ 
+                                console.log("ok")
+                                module.exports = { results: results}
+                                socket.emit('one', results);
+                                console.log("socket ok")
+                    
+                                
+                            })
+                    
+                            db.close()
+                        })
+                        
+                        
+                    
+                    });
+                }, 500);
+                
+            }
+            
+        
+    }
 }
